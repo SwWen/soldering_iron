86eec8fb0788a717c0bdc6331ce685b73a18da09
Anton Vodovozov <rd3tal@gmail.com>
2019-09-14 21:53:31 +0300

    run and debug config

--- Src/main.c
+++ Src/main.c
@@ -2,12 +2,16 @@
 #include "main.h"
 #include"stm32f1xx.h"
 int main(void)
+
+
 {
 
-
+RCC->APB2ENR|=RCC_APB2ENR_IOPCEN;
+GPIOC->CRH|=GPIO_CRH_MODE13_0;
   while (1)
   {
-
+GPIOC->BSRR|=GPIO_BSRR_BR13;
+GPIOC->BSRR|=GPIO_BSRR_BS13;
   }
 }
 
