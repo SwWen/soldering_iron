9fd986a096263d8bc9bcb716c454334a01d6b62d
Anton Vodovozov <rd3tal@gmail.com>
2019-09-14 20:28:34 +0300

    bare project

---
+++ Src/main.c
@@ -0,0 +1,32 @@
+
+#include "main.h"
+#include"stm32f1xx.h"
+int main(void)
+{
+
+
+  while (1)
+  {
+
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void Error_Handler(void)
+{
+
+}
+
---
+++ Src/stm32f1xx_it.c
@@ -0,0 +1,96 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    stm32f1xx_it.c
+  * @brief   Interrupt Service Routines.
+  ******************************************************************************
+  * @attention
+  *
+  * <h2><center>&copy; Copyright (c) 2019 STMicroelectronics.
+  * All rights reserved.</center></h2>
+  *
+  * This software component is licensed by ST under BSD 3-Clause license,
+  * the "License"; You may not use this file except in compliance with the
+  * License. You may obtain a copy of the License at:
+  *                        opensource.org/licenses/BSD-3-Clause
+  *
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "stm32f1xx_it.h"
+void NMI_Handler(void)
+{
+
+}
+
+
+void HardFault_Handler(void)
+{
+
+  while (1)
+  {
+
+  }
+}
+
+void MemManage_Handler(void)
+{
+
+  while (1)
+  {
+
+  }
+}
+
+
+void BusFault_Handler(void)
+{
+
+  while (1)
+  {
+
+  }
+}
+
+/**
+  * @brief This function handles Undefined instruction or illegal state.
+  */
+void UsageFault_Handler(void)
+{
+
+  while (1)
+  {
+
+  }
+}
+
+
+void SVC_Handler(void)
+{
+
+}
+
+
+void DebugMon_Handler(void)
+{
+
+}
+
+void PendSV_Handler(void)
+{
+
+}
+
+/**
+  * @brief This function handles System tick timer.
+  */
+void SysTick_Handler(void)
+{
+
+}
+
+
+/******************************************************************************/
+
